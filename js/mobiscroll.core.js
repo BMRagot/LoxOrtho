625821aedfafb14a1ccc935412c607ca414e8369
BMRagot <bastien.marichal@gmail.com>
2014-03-14 16:09:30 +0100

    group files

--- js/mobiscroll.core.js
+++ js/mobiscroll.core.js
@@ -126,3 +126,67 @@
     $.fn.scroller = $.fn.scroller || $.fn.mobiscroll;
 
 })(jQuery);
+(function ($) {
+    $.mobiscroll.i18n.fr = $.extend($.mobiscroll.i18n.fr, {
+        // Core
+        setText: 'Terminé',
+        cancelText: 'Annuler',
+        clearText: 'Effacer',
+        selectedText: 'Sélectionné',
+        // Datetime component
+        dateFormat: 'dd/mm/yy',
+        dateOrder: 'ddmmyy',
+        dayNames: ['&#68;imanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        dayNamesShort: ['&#68;im.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+        dayText: 'Jour',
+        monthText: 'Mois',
+        monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        hourText: 'Heures',
+        minuteText: 'Minutes',
+        secText: 'Secondes',
+        timeFormat: 'HH:ii',
+        timeWheels: 'HHii',
+        yearText: 'Année',
+        nowText: 'Maintenant',
+        pmText: 'après-midi',
+        amText: 'avant-midi',
+        // Calendar component
+        dateText: 'Date',
+        timeText: 'Heure',
+        calendarText: 'Calendrier',
+        closeText: 'Fermer',
+        // Daterange component
+        fromText: 'Démarrer',
+        toText: 'Fin',
+        // Measurement components
+        wholeText: 'Entier',
+        fractionText: 'Fraction',
+        unitText: 'Unité',
+        // Time / Timespan component
+        labels: ['Ans', 'Mois', 'Jours', 'Heures', 'Minutes', 'Secondes', ''],
+        labelsShort: ['Yrs', 'Mths', 'Days', 'Hrs', 'Mins', 'Secs', ''],
+        // Timer component
+        startText: 'Démarrer',
+        stopText: 'Arrêter',
+        resetText: 'Réinitialiser',
+        lapText: 'Lap',
+        hideText: 'Cachez'
+    });
+})(jQuery);
+(function ($) {
+
+    $.mobiscroll.themes.ios7 = {
+        dateOrder: 'MMdyy',
+        rows: 5,
+        height: 34,
+        minWidth: 55,
+        headerText: false,
+        showLabel: false,
+        btnWidth: false,
+        selectedLineHeight: true,
+        selectedLineBorder: 1,
+        useShortLabels: true
+    };
+
+})(jQuery);
